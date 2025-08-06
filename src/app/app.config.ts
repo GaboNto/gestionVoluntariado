1b8c06500568a30157e17182927d5bff781639e6
Gabriel Pailamilla <87296844+GaboNto@users.noreply.github.com>
2025-08-05 23:33:26 -0400

    fixeo bd

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { SHARED_IMPORTS } from './shared-imports'; // 👈 nuevo
+import { SHARED_IMPORTS } from './shared-imports';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    SHARED_IMPORTS // 👈 incluimos aquí los imports comunes
+    SHARED_IMPORTS
   ]
 };
--- src/app/components/ofertas/oferta.service.ts
+++ src/app/components/ofertas/oferta.service.ts
@@ -31,7 +31,6 @@ export class OfertaService {
     });
   }
 
-  // GET: Obtener ofertas registradas
   getOfertas(): Observable<Oferta[]> {
     return this.http.get<Oferta[]>(this.apiUrl);
   }
